cff46cd1f098b05f97b533ad2b3e7eb1c3b8f4f4
tic@K42Jv <tic@K42Jv>
2012-05-12 02:19:09 +0800

    hide read chroot path, and reserve upper path after chroot.

--- bin/overlayfs_chroot.sh
+++ bin/overlayfs_chroot.sh
@@ -3,18 +3,20 @@
 pid=$$
 export debian_chroot=chroot_$pid
 
-shadow_root=/tmp/chroot/$pid
+upper_root=/tmp/chroot/$pid
+shadow_root=/tmp/chroot/.$pid
 
 ## remove last one if exist
-if [ -d $shadow_root ];then
-	rm -rf "$shadow_root";
+if [ -d $upper_root ];then
+	rm -rf "$upper_root";
 fi
 
 ## create rootfs
+mkdir -p $upper_root
 mkdir -p $shadow_root
 
 ## chroot script
-cat << EOF > $shadow_root/.chroot.sh
+cat << EOF > $upper_root/.chroot.sh
 mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 mount -t devtmpfs devtmpfs /dev
@@ -27,10 +29,15 @@ umount /sys
 umount /proc
 EOF
 
-
 #### START CHROOT HERE ####
-mount -t overlayfs -o lowerdir=/,upperdir=$shadow_root overlayfs $shadow_root
+mount -t overlayfs -o lowerdir=/,upperdir=$upper_root overlayfs $shadow_root
 chroot $shadow_root sh -e /.chroot.sh
 
 ## exit
-umount $shadow_root
+if (umount $shadow_root > /dev/null 2>&1) ; then
+	echo -n "umount safe: "
+else
+	echo -n "umount fail: "
+fi
+
+echo "checkout [$upper_root]."
